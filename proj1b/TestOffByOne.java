671efa3fb2a186a57df4cb37b594cd1b495716bc
Daniel Vazquez Guevara <dvazquez@berkeley.edu>
2019-02-10 19:38:31 -0800

    reduced test to testEqualsChars in TestOffByOne.java

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -12,18 +12,12 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('x', 'y'));
         assertFalse(offByOne.equalChars('a', 'a'));
         assertFalse(offByOne.equalChars('a', 'c'));
-        assertTrue(offByOne.equalChars('a', 'b'));
-        assertTrue(offByOne.equalChars('w', 'x'));
-        assertTrue(offByOne.equalChars('r', 'q'));
-        assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'B'));
-        assertFalse(offByOne.equalChars('Y', 'z'));
         assertTrue(offByOne.equalChars('K', 'L'));
         assertTrue(offByOne.equalChars('&', '%'));
         assertFalse(offByOne.equalChars('$', '+'));
         assertFalse(offByOne.equalChars('D', 'D'));
         assertFalse(offByOne.equalChars('=', '='));
         assertFalse(offByOne.equalChars('a', '#'));
-        assertTrue(offByOne.equalChars('1', '2'));
     }
 }
