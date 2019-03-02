ef1cf75820f498caae9ff74dabd103f07624067c
Daniel Vazquez Guevara <dvazquez@berkeley.edu>
2019-03-01 19:35:20 -0800

    lab 6 completed

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,35 +1,43 @@
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    // instance variables
+    private int[] parent;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        parent = new int[n];
+        // all element are disconnected at the beginning
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex < 0 || vertex >= parent.length){
+            throw new ArrayIndexOutOfBoundsException("Invalid index");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return -1 * parent[find(v1)]; // find returns root index, which stores size
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return parent[v1]; // returns the vertex's parent
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        validate(v1);
+        validate(v2);
+        return find(v1) == find(v2); // check if the share root
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,14 +46,34 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        validate(v1);
+        validate(v2);
+        // find corresponding root of each set
+        int r1 = find(v1);
+        int r2 = find(v2);
+        int size1 = sizeOf(r1);
+        int size2 = sizeOf(r2);
+        // if vertices not in same set, then join smaller tree to larger
+        if (r1 != r2) {
+            int new_size = - (size1 + size2);
+            if (size1 <= size2) {
+                parent[r1] = r2; // join r1's set to r2's set
+                parent[r2] = new_size; // increase size of new larger tree
+            } else {
+                parent[r2] = r1; // join r2's set to r1's set
+                parent[r1] = new_size; // increase size of new larger tree
+            }
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        int r = vertex;
+        while (parent[r] >= 0) {
+            r = parent[r];
+        }
+        return r;
     }
 
 }
---
+++ lab6/testUnionFind.java
@@ -0,0 +1,54 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class testUnionFind {
+    @Test
+    public void testConnected() {
+        UnionFind dset = new UnionFind(16);
+        assertEquals(1, dset.sizeOf(1));
+        dset.union(3, 2);
+        assertEquals(2, dset.find(3));
+        dset.union(8, 9);
+        assertEquals(2, dset.sizeOf(9));
+        dset.union(2, 8);
+        dset.union(2, 4);
+        assertEquals(9, dset.find(3));
+        assertEquals(5, dset.sizeOf(8));
+        dset.union(5, 6);
+        dset.union(6, 7);
+        assertEquals(6, dset.find(7));
+        assertEquals(3, dset.sizeOf(6));
+        dset.union(7, 8);
+        assertEquals(8, dset.sizeOf(5));
+        assertTrue(dset.connected(7, 3));
+        assertEquals(1, dset.sizeOf(15));
+        dset.union(1, 14);
+        dset.union(13, 15);
+        dset.union(15, 1);
+        assertEquals(14, dset.find(1));
+        assertEquals(4, dset.sizeOf(13));
+        dset.union(8, 13);
+        assertTrue(dset.connected(1, 5));
+        assertEquals(9, dset.find(1));
+        assertEquals(12, dset.sizeOf(13));
+        assertFalse(dset.connected(0, 10));
+        dset.union(12, 11);
+        dset.union(10, 0);
+        dset.union(0, 12);
+        assertEquals(11, dset.find(10));
+        assertEquals(4, dset.sizeOf(12));
+        assertTrue(dset.connected(10, 12));
+        assertFalse(dset.connected(7, 10));
+        dset.union(10, 13);
+        assertEquals(9, dset.find(12));
+        assertEquals(16, dset.sizeOf(3));
+        assertTrue(dset.connected(7, 10));
+
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testValidIndex() {
+        UnionFind dset = new UnionFind(16);
+        dset.sizeOf(-1);
+    }
+}
