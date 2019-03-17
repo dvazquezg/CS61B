d21a4a07d9e9de4260a5184e904cd46363483c1d
Daniel Vazquez Guevara <dvazquez@berkeley.edu>
2019-03-16 17:25:45 -0700

    Finished challenge lab 8

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -7,14 +9,47 @@ import java.util.ArrayList;
  * considered to be in the air at the same time.
  */
 public class FlightSolver {
+    PriorityQueue<Flight> endTimes;
+    PriorityQueue<Flight> startTimes;
+    int maxPassengersOnAir;
 
     public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
+        // Constructors using lambda functions
+        Comparator<Flight> smallerEndtime = (Flight f1, Flight f2) -> {
+            int diff = f1.endTime() - f2.endTime();
+            return diff;
+        };
+        Comparator<Flight> smallerStartime = (Flight f1, Flight f2) -> {
+            int diff = f1.startTime() - f2.startTime();
+            return diff;
+        };
+        // initialize priority queues
+        endTimes = new PriorityQueue<>(smallerEndtime);
+        startTimes = new PriorityQueue<>(smallerStartime);
+
+        for (Flight f: flights) {
+            startTimes.add(f);
+            endTimes.add(f);
+        }
+        maxPassengersOnAir = 0;
+
     }
 
     public int solve() {
-        /* FIX ME */
-        return -1;
+        //every time that a plane takes off add number of passengers
+        //every time that a plane lands subtract number of passengers
+        int passengersOnAir = 0;
+        while (startTimes.size() != 0 && endTimes.size() != 0) {
+            if (startTimes.peek().startTime() <= endTimes.peek().endTime()) {
+                passengersOnAir += startTimes.remove().passengers();
+            } else {
+                passengersOnAir -= endTimes.remove().passengers();
+            }
+            if (passengersOnAir > maxPassengersOnAir) {
+                maxPassengersOnAir = passengersOnAir;
+            }
+        }
+        return maxPassengersOnAir;
     }
 
 }
